d45afdb0cc31e7ebef6adb5d9e8ffbd995267f2c
Pranav Gurditta <pranavgurditta@gmail.com>
2020-10-31 01:05:48 +0530

    Create SolutionByPranavGurditta.java

---
+++ Basic/Check Leap Year/SolutionByPranavGurditta.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Leap_year
+{
+    public static void main()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a year to check if it is a leap year.");
+        int n=sc.nextInt();
+        if(n<0)
+        {
+            System.out.println("Enter a positive number.");
+            n=sc.nextInt();
+        }
+        Leap_year obj=new Leap_year();
+        if(obj.leap_year(n))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+    public boolean leap_year(int n)
+    {
+        if(n%4==0)
+            return true;
+        else
+            return false;
+    }
+    
+}
